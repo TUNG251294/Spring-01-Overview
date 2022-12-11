0b99f6bf27be14edef38779fb8f99b676c708131
TUNG251294 <you@example.comhuynhthanhtunga1@gmail.com git config --global user.name Your>
2022-12-12 05:59:17 +0700

    commit03

--- SpringGreeting/src/main/java/com/example/controller/GreetingController.java
+++ SpringGreeting/src/main/java/com/example/controller/GreetingController.java
@@ -1,4 +1,5 @@
-package com.example.demo.controller;
+package com.example.controller;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
     @GetMapping("/greeting")
-    public String greeting(@RequestParam String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam String name, Model model){
+        model.addAttribute("name",name);
         return "index";
     }
 }
